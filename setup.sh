548bfb1db9b3178a877b6528743a87e3633b94f7
Gladiator07 <atharvaaingle@gmail.com>
2021-05-11 17:54:20 +0000

    initial setup

---
+++ setup.sh
@@ -0,0 +1,21 @@
+
+echo "Installing dependencies"
+pip3 install --upgrade --force-reinstall --no-deps kaggle
+
+# Put your Kaggle api key path here
+echo "Fetching your Kaggle API Key"
+kaggle_api_key_path='/content/drive/MyDrive/Kaggle/kaggle.json'
+
+# This snippet will install kaggle api and connect your api-key to it
+mkdir -p ~/.kaggle
+echo "Setting up your Kaggle key to API..."
+cp $kaggle_api_key_path ~/.kaggle/
+cat ~/.kaggle/kaggle.json
+chmod 600 ~/.kaggle/kaggle.json
+echo "Kaggle API Key successfully linked !!!"
+
+cd /content/GPU-vs-TPU
+mkdir input/
+cd input/
+kaggle competitions download -c tpu-getting-started
+unzip tpu-getting-started.zip
