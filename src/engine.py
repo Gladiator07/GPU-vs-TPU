cb34e05b6e211f6f7fea11f0f45ed53e7247cf62
Gladiator07 <atharvaaingle@gmail.com>
2021-05-12 10:44:02 +0000

    engine

--- src/engine.py
+++ src/engine.py
@@ -18,4 +18,21 @@ class Engine:
         scheduler=None,
         tpu=False
     ):
+        model.train()
+        if tpu:
+            para_loader = pl.ParallelLoader(data_loader, [device])
+            tk0 = tqdm(para_loader.per_device_loader(device), total=len(data_loader))
+        else:    
+            tk0 = tqdm(data_loader, total=len(data_loader))
+        for bi, data in enumerate(tk0):
+            for k,v in data.items():
+                data[k] = v.to(device)
+        
+        _, loss = model(**data)
+
+        loss.backward()
+        optimizer.step()
+        if scheduler is not None:
+            scheduler.step()
+        
         
